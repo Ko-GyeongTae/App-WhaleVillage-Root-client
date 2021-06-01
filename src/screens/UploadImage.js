002f112cc3719e9bea8000668d6de9649b8d7d54
Ko-GyeongTae <kokt0203@naver.com>
2021-06-02 08:10:51 +0900

    <EDIT> Connect to Upload API

--- src/screens/UploadImage.js
+++ src/screens/UploadImage.js
@@ -1,154 +1,111 @@
-/*
-import * as MediaLibrary from 'expo-media-library';
 import React, { useState, useEffect } from 'react';
-import { Ionicons } from "@expo/vector-icons";
-import {
-    View,
-    Text,
-    StyleSheet,
-    FlatList,
-    Image,
-    TouchableOpacity,
-    useWindowDimensions,
-} from "react-native";
+import { Button, Image, View, Platform, TouchableOpacity, Text, Alert, StyleSheet } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
 import { baseUri } from '../../env';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default ({ navigation }) => {
-    const [ok, setOk] = useState(false);
-    const [photos, setPhotos] = useState([]);
-    const [chosenPhoto, setChosenPhoto] = useState("");
-    const getPhotos = async () => {
-        const { assets: photos } = await MediaLibrary.getAssetsAsync();
-        console.log(photos)
-        setPhotos(photos);
-        setChosenPhoto(photos[0]);
-    };
-    const GetToken = async () => {
-        const token = await AsyncStorage.getItem("jwt");
-        return token;
-    };
-    const getPermissions = async () => {
-        const {
-            accessPrivileges,
-            canAskAgain,
-        } = await MediaLibrary.getPermissionsAsync();
-        if (accessPrivileges === "none" && canAskAgain) {
-            const { accessPrivileges } = await MediaLibrary.requestPermissionsAsync();
-            if (accessPrivileges !== "none") {
-                setOk(true);
-                getPhotos();
-            }
-        } else if (accessPrivileges !== "none") {
-            setOk(true);
-            getPhotos();
-        }
-    };
-    const uploadImage = async() => {
-        const formData = new FormData();
-        const token = await GetToken();
-
-        const config = {
-            headers: { 
-                Accept: 'application/json',
-                'Content-Type': 'multipart/form-data',
-                authentication: token 
-            },
+export default function ImagePickerExample({navigation}) {
+    const [image, setImage] = useState(null);
+    const [uploadedImage, setUploadedImage] = useState([]);
+    let uploaded = [];
+    const uploadImage = async () => {
+        const GetToken = async () => {
+            const token = await AsyncStorage.getItem("jwt");
+            return token;
         };
-        console.log(chosenPhoto);
-        formData.append("media", chosenPhoto.uri);
+        const token = await GetToken();
+       
+        const xhr = new XMLHttpRequest();
+        const formData = new FormData();
+        formData.append('media', {
+            uri: image.uri,
+            type: 'image/jpeg',
+            name: 'photo.jpg',
+        });
+        xhr.open('POST', `${baseUri.outter_net}/api/v1/media`);
+        xhr.setRequestHeader('authentication', token);
+        xhr.send(formData);
+        xhr.addEventListener("progress", updateProgress);
+        xhr.addEventListener("load", transferComplete);
+        xhr.addEventListener("error", transferFailed);
+        xhr.addEventListener("abort", transferCanceled);
+        // progress on transfers from the server to the client (downloads)
+        function updateProgress (oEvent) {
+          if (oEvent.lengthComputable) {
+            return <Text>Loading</Text>
+            // ...
+          } else {
+            // Unable to compute progress information since the total size is unknown
+          }
+        }
 
-        await axios.post(`${baseUri.outter_net}/api/v1/media`, formData, config)
-            .then(res => {
-                console.log(res.data);
-                navigation.navigate("WritePost", res.data);
-            })
-            .catch(e => {
-                console.log(e.response.data);
-            });
+        function transferComplete(evt) {
+          console.log(xhr.response);
+          Alert.alert('성공적으로 업로드했습니다.');
+          uploaded.append(xhr.response);
+          console.log(uploaded);
+          setImage(null);
+          console.log("The transfer is complete.");
+        }
+
+        function transferFailed(evt) {
+          console.log("An error occurred while transferring the file.");
+        }
+
+        function transferCanceled(evt) {
+          console.log("The transfer has been canceled by the user.");
+        }
+
     }
     const HeaderRight = () => (
-        <TouchableOpacity
-            onPress={() =>
-                uploadImage()
-            }
-        >
+        <TouchableOpacity onPress={() => {
+            setUploadedImage(uploaded);
+            navigation.navigate("WritePost", uploadedImage);
+        }}>
             <Text style={FontStyle.HeaderRightText}>Next</Text>
         </TouchableOpacity>
     );
     useEffect(() => {
-        getPermissions();
-    }, []);
-    useEffect(() => {
         navigation.setOptions({
             headerRight: HeaderRight,
         });
+        (async () => {
+            if (Platform.OS !== 'web') {
+                const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+                if (status !== 'granted') {
+                    alert('Sorry, we need camera roll permissions to make this work!');
+                }
+            }
+        })();
+        return console.log(uploadedImage);
     }, []);
-    const numColumns = 4;
-    const { width } = useWindowDimensions();
-    const choosePhoto = (uri) => {
-        console.log(uri);
-        setChosenPhoto(uri);
+
+    const pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+        });
+
+        console.log(result);
+
+        if (!result.cancelled) {
+            setImage(result);
+        }
     };
-    const renderItem = ({ item: photo }) => (
-        console.log(photo),
-        <TouchableOpacity onPress={() => choosePhoto(photo)}>
-            <Image
-                source={{ uri: photo.uri }}
-                style={{ width: width / numColumns, height: 100 }}
-            />
-            <View style={Style.IconContainer}>
-                <Ionicons
-                    name="checkmark-circle"
-                    size={18}
-                    color={photo.uri === chosenPhoto.uri ? 'blue' : "white"}
-                />
-            </View>
-        </TouchableOpacity>
-    );
+
     return (
-        <View style={Style.Container}>
-            <View style={Style.Top}>
-                {chosenPhoto !== "" ? (
-                    <Image
-                        source={{ uri: chosenPhoto.uri }}
-                        style={{ width, height: "100%" }}
-                    />
-                ) : null}
-            </View>
-            <View style={Style.Bottom}>
-                <FlatList
-                    data={photos}
-                    numColumns={numColumns}
-                    keyExtractor={(photo) => photo.id}
-                    renderItem={renderItem}
-                />
-            </View>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Button title="Pick an image from camera roll" onPress={pickImage} />
+            {image && <Image source={{ uri: image.uri }} style={{ width: 200, height: 200 }} />}
+            <TouchableOpacity onPress={() => uploadImage()}>
+                <Text>Upload!</Text>
+            </TouchableOpacity>
         </View>
     );
 }
-
-const Style = StyleSheet.create({
-    Container: {
-        flex: 1,
-        backgroundColor: 'black',
-    },
-    Top: {
-        flex: 1,
-        backgroundColor: 'black',
-    },
-    Bottom: {
-        flex: 1,
-        backgroundColor: 'black',
-    },
-    IconContainer: {
-        position: 'absolute',
-        bottom: 5,
-        right: 0,
-    }
-});
-
 const FontStyle = StyleSheet.create({
     HeaderRightText: {
         color: 'blue',
@@ -156,77 +113,3 @@ const FontStyle = StyleSheet.create({
         marginRight: 7,
     }
 })
-*/
-import React, { useState, useEffect } from 'react';
-import { Button, Image, View, Platform, TouchableOpacity, Text } from 'react-native';
-import * as ImagePicker from 'expo-image-picker';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import axios from 'axios';
-import { baseUri } from '../../env';
-
-export default function ImagePickerExample() {
-  const [image, setImage] = useState(null);
-  const uploadImage = async() => {
-    const formData = new FormData();
-    const GetToken = async () => {
-        const token = await AsyncStorage.getItem("jwt");
-        return token;
-    };
-    const token = await GetToken();
-
-    const config = {
-        headers: { 
-            Accept: 'application/json',
-            'Content-Type': 'multipart/form-data',
-            authentication: token 
-        },
-    };
-    console.log(image.uri);
-    formData.append('media', image.uri);
-    console.log(formData);
-
-    await axios.post(`${baseUri.outter_net}/api/v1/media`, formData, config)
-        .then(res => {
-            console.log(res.data);
-            navigation.navigate("WritePost", res.data);
-        })
-        .catch(e => {
-            console.log(e.response.data);
-        });
- }
-  useEffect(() => {
-    (async () => {
-      if (Platform.OS !== 'web') {
-        const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-        if (status !== 'granted') {
-          alert('Sorry, we need camera roll permissions to make this work!');
-        }
-      }
-    })();
-  }, []);
-
-  const pickImage = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
-    });
-
-    console.log(result);
-
-    if (!result.cancelled) {
-      setImage(result);
-    }
-  };
-
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button title="Pick an image from camera roll" onPress={pickImage} />
-      {image && <Image source={{ uri: image.uri }} style={{ width: 200, height: 200 }} />}
-      <TouchableOpacity onPress={() => uploadImage()}>
-          <Text>Upload!</Text>
-      </TouchableOpacity>
-    </View>
-  );
-}
