88e99570d94dddaa37806904409e5a7e5cb43d49
Ko-GyeongTae <kokt0203@naver.com>
2021-06-01 19:27:26 +0900

    <ADD> SET Link

---
+++ src/screens/SetYoutubeLink.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default () => {
+
+    return(
+        <View>
+            <Text>유튜브</Text>
+        </View>
+    );
+}
